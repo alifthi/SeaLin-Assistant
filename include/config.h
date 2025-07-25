5657d8b1d43702462d4ea830352b3e2885fc07c0
alifathi <alifathi8008@gmail.com>
2025-07-25 12:29:10 +0330

    Basic tool config added

--- include/config.h
+++ include/config.h
@@ -5,24 +5,28 @@
 
 // Prompt configs
 #define MAX_MESSAGE_LENGTH 2048*4
-#define MAX_SEARCH_QUERY_LENGTH 256
 #define SYSTEM_PROMPT "You are an AI assistant to help user to do the tasks that user asks with there linux os.\n\
-                        If users asks for something that can handle with there linux at first you must generate `run_command` token at the begining of generation.\n\
+                        If users asks for something that can handle with there linux at first you must generate `shell_command` token at the begining of generation.\n\
                         then you must generate a Code like follow\n\
                         ```bash \n<your code> \n```\n\
                         Do not generate any fallback response when you generate a shell code.\n\
                         Do not generate more than one code block in each round of response.\n\
                         After executing the provided code you will recive the result of execution, then if it was successfull you should generate a fallback response else you must run another code if it's possible\n\
-                        Every time user asked for doing something you must generate `shell_node` token before code.\n\
+                        Every time user asked for doing something you must generate `shell_command` token before code.\n\
                         You are able to run multiple code on users linux and see the results then run another code.\n\
                         each time you want to generate code only one batch of code are allowed, so do not generate multiple codes in each round of generation\n\
                         If it's not possible to run another code you mut tell user that it's not possible and why.\n\
-                        You are able to search in web for that you must only generate `search_node` token\n\
-                        After that you generate the search_node you must provide the search query like follow:\n\
+                        You are able to search in web for that you must only generate `search_request` token\n\
+                        After that you generate the search_request you must provide the search query like follow:\n\
                         ```query \n<your search query>\n'''\n\
                         If you need to search about your code or search about what user asked use this tool.\n\
                         If user didn't ask for doing anything with there linux OS only chat normally.\n"
 
+// Tools configs
+#define MAX_STDOUT_LENGTH 256
+#define MAX_STDERR_LENGTH 256
+#define MAX_SEARCH_QUERY_LENGTH 256
+
 // Special tokens
-#define search_request_token "<search_request>"
-#define run_command_token "<run_command>"
+#define search_request_token "search_request"
+#define run_command_token "shell_command"
