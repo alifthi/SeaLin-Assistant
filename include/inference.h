b6bcf2d34aa76925fb069ee6d9bd3fb18e804f10
alifathi <alifathi8008@gmail.com>
2025-07-25 22:06:48 +0330

    Some comments and function defination added

--- include/inference.h
+++ include/inference.h
@@ -3,18 +3,30 @@
 
 #include "config.h"
 #include "states.h"
+#include <string.h> 
 
+// Forwarding types
 typedef struct llama_model llama_model_forwarded;
 typedef struct llama_vocab llama_vocab_forwarded;
 typedef struct llama_context llama_context_fowarded;
+typedef struct llama_tokens llama_tokens_forwarded;
 
-
+// To load llama cpp backend
 void load_backend();
 
+// To load model
 llama_model_forwarded* load_model(state_type state);
 
+// To get vocab
 llama_vocab_forwarded* get_vocab(llama_model_forwarded model);
 
+// To get number of tokens in a prompt
+int get_num_tokens(llama_vocab_forwarded vocab,char *prompt);
+
+// Allocate a memory to prompt
+llama_tokens_forwarded allocate_prompt(int n_prompt, llama_tokens_forwarded prompt_tokens, char *prompt,llama_vocab_forwarded vocab);
+
+// Create a llama context
 llama_context_fowarded* create_ctx();
 
 #endif
