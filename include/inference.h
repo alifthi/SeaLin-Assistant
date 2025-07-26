707e0a880da5509247034b58d17110acda610829
alifathi <alifathi8008@gmail.com>
2025-07-26 09:34:35 +0330

    A type defined to store any variable related to llamacpp

--- include/inference.h
+++ include/inference.h
@@ -11,22 +11,31 @@ typedef struct llama_vocab llama_vocab_forwarded;
 typedef struct llama_context llama_context_fowarded;
 typedef struct llama_tokens llama_tokens_forwarded;
 
+// llama_inference type contains any variable related to llamacpp
+typedef struct 
+{
+    llama_model_forwarded* model;
+    llama_context_fowarded* ctx;
+    llama_vocab_forwarded* vocab;
+    int n_prompt;
+}llama_inference;
+
 // To load llama cpp backend
 void load_backend();
 
 // To load model
-llama_model_forwarded* load_model(state_type state);
+int load_model(char *path, llama_inference* inference);
 
 // To get vocab
-llama_vocab_forwarded* get_vocab(llama_model_forwarded model);
+int get_vocab(llama_inference* inference);
 
 // To get number of tokens in a prompt
-int get_num_tokens(llama_vocab_forwarded vocab,char *prompt);
+int get_num_tokens(llama_inference* inference, state_type* state);
 
 // Allocate a memory to prompt
-llama_tokens_forwarded allocate_prompt(int n_prompt, llama_tokens_forwarded prompt_tokens, char *prompt,llama_vocab_forwarded vocab);
+int allocate_prompt(llama_inference* inference, state_type* state);
 
 // Create a llama context
-llama_context_fowarded* create_ctx();
+int create_ctx(llama_inference* inference);
 
 #endif
