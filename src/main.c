f7e21d8fdb55f55b8d1093baeca08dfb0c65dedb
alifathi <alifathi8008@gmail.com>
2025-07-27 13:55:46 +0330

    now we can just inference

--- src/main.c
+++ src/main.c
@@ -13,6 +13,7 @@ int main(){
     memset(&inference, 0, sizeof(llama_inference));
     memset(&state, 0, sizeof(state_type));
 
+    init_default_state(&state);
     load_backend();
 
     int res = load_model(DEFAULT_MODEL_PATH, &inference);
@@ -28,6 +29,31 @@ int main(){
         return 1;
     }
 
+    res = allocate_prompt(&inference, &state);
+    if(res){
+        free_llama_inference(&inference);
+        return 1;
+    }
+
+    res = create_ctx(&inference);
+    if(res){
+        free_llama_inference(&inference);
+        return 1;
+    }
+
+    res = set_sampler(&inference);
+    if(res){
+        free_llama_inference(&inference);
+        return 1;
+    }
+
+    res = run_inference(&inference);
+    if(res){
+        free_llama_inference(&inference);
+        return 1;
+    }
+    free_ptr(&state);
+    printf(state.messages);
     free_llama_inference(&inference);
     return 0;
 }
