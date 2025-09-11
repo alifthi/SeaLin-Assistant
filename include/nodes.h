2be350c73517ef7400ddc04ad0a897202565a2b5
alifathi <alifathi8008@gmail.com>
2025-09-11 11:53:04 +0330

    dependency issue resolved

--- include/nodes.h
+++ include/nodes.h
@@ -2,7 +2,7 @@
 #define NODES_H
 
 #include "states.h"
-
+#include "inference.h"
 char* decide_next_node(state_type state);
 char* split(const char* main_string, const char* needle);
 int inference_node(state_type *state, llama_inference *inference, char *assistant_response);
