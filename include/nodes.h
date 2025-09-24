5f2a46e65651e7330d97c3164c792f8f9c231e9e
alifathi <alifathi8008@gmail.com>
2025-09-24 11:50:11 +0330

    split function changed in a way to returns the section that user says and also prepare_cmd is developed

--- include/nodes.h
+++ include/nodes.h
@@ -4,7 +4,7 @@
 #include "states.h"
 #include "inference.h"
 char* decide_next_node(state_type state);
-char* split(const char* main_string, const char* needle);
+char* split(const char* main_string, const char* needle, short index);
 int inference_node(state_type *state, llama_inference *inference);
 int search_node(state_type *state);
 int prepare_cmd(state_type* state);
--- src/nodes.c
+++ src/nodes.c
@@ -6,8 +6,7 @@
     * @return: An string that tells what the next node is
 */
 char* decide_next_node(state_type state){
-    char* splited = split(state.assistant_response, "</think>");
-    printf(splited);
+    char* splited = split(state.assistant_response, "</think>",-1);
     if(strstr(splited, "shell_command") != NULL)
         return "shell_node";
     else if(strstr(splited, "search_request") != NULL)
@@ -34,17 +33,56 @@ int inference_node(state_type *state, llama_inference *inference){
 }
 
 /*
-    * Finds the last occurrence of needle in haystack.
-    * @param haystack: The main string.
-    * @param needle.
-    * @return: The last occurrence of needle.
+    * Preparing command and store in state->shell_command.
+    * @param state: Graph state.
+    * @return: returns 0 if it was successful.
 */
-char* split(const char* haystack, const char* needle) {
-    char *result;
-    char *p = strstr(haystack, needle);
-    while (p) {
-        result = p;
-        p = strstr(p + 1, needle);
+int prepare_cmd(state_type *state){
+    char* splited = split(state->assistant_response, "</think>",-1);
+
+    char* temp = split(splited, "```bash",-1);
+    free(splited);
+    splited = temp;
+    temp = split(splited, "```", 0);
+    free(splited);
+    splited = temp;
+    
+    return 0;
+}
+
+
+/*
+    * Split haystack by needle and return section at index.
+    * @param haystack: The main string.
+    * @param needle: The delimiter.
+    * @param index: Section index to return (-1 for last).
+    * @return: The section at specified index.
+*/
+char* split(const char* haystack, const char* needle, short index) {
+    const char *start = haystack;
+    const char *start_tmp = haystack;
+    const char *p;
+    size_t dlen = strlen(needle);
+    int i = 0;
+
+    while (1) {
+        p = strstr(start_tmp, needle);
+        if(index==-1 && !p || i==index)
+            break;
+        if (!p) {
+            return NULL;
+        }
+        start = start_tmp;
+        start_tmp = p + dlen; 
+        i++;
     }
-    return result;
+
+    p = strstr(start, needle);
+    size_t len = p ? (size_t)(p - start) : strlen(start);
+
+    char *token = malloc(len + 1);
+    if (!token) return NULL;
+    memcpy(token, start_tmp, len);
+    token[len] = '\0';
+    return token;
 }
